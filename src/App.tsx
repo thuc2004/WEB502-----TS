f50b5a7fe986f386f4a8d62aa2986f0834e59c42
thuc2004 <151771238+thuc2004@users.noreply.github.com>
2026-01-14 19:51:59 +0700

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { Toaster } from "react-hot-toast";
-import { Link } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import ListPage from "./pages/List";
+import EditPage from "./pages/Edit";
+import AddPage from "./pages/Add";
 
 function App() {
   return (
     <>
       <nav className="bg-blue-600 text-white shadow">
         <div className="max-w-6xl mx-auto px-4 h-16 flex items-center justify-between">
-          <Link to="#" className="text-xl font-semibold">
+          <Link to="/" className="text-xl font-semibold">
             <strong>WEB502 App</strong>
           </Link>
 
           <div className="hidden md:flex items-center space-x-8">
-            <Link to="#" className="hover:text-gray-200">
+            <Link to="edit" className="hover:text-gray-200">
               Trang chủ
             </Link>
-            <Link to="#" className="hover:text-gray-200">
+            <Link to="add" className="hover:text-gray-200">
               Danh sách
             </Link>
             <Link to="#" className="hover:text-gray-200">
@@ -37,7 +40,11 @@ function App() {
       <div className="max-w-6xl mx-auto mt-10 px-4 text-center">
         <h1 className="text-4xl font-bold mb-4">Chào mừng đến với WEB502</h1>
       </div>
-
+      <Routes>
+        <Route path="/" element={<ListPage />} />
+        <Route path="/edit" element={<EditPage />} />
+        <Route path="/add" element={<AddPage />} />
+      </Routes>
       <Toaster />
     </>
   );
